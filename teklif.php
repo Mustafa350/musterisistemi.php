dc98d8c0109f47719cae10989d4e97d41cd258df
Mustafa Ertan <46426293+Mustafa350@users.noreply.github.com>
2019-04-01 18:38:00 +0300

    Create teklif.php

---
+++ teklif.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <script language="Javascript" type="text/javascript" src="js/jquery-1.4.1.js"></script>
+    <script language="Javascript" type="text/javascript" src="js/jquery.lwtCountdown-1.0.js"></script>
+    <script language="Javascript" type="text/javascript" src="js/misc.js"></script>
+    <link rel="Stylesheet" type="text/css" href="style/main.css"></link>
+    <style media="screen">
+      table{
+        width: 75%;
+      }
+      table, th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+      }
+      th, td {
+      padding: 15px;
+      text-align: left;
+      }
+      table#t01 tr:nth-child(even) {
+      background-color: #eee;
+      }
+      table#t01 tr:nth-child(odd) {
+      background-color: #fff;
+      }
+      table#t01 th {
+      background-color: #676a6a;
+      color: white;
+      }
+    </style>
+    <meta charset="utf-8">
+    <title>Teklifler </title>
+  </head>
+  <body>
+    <table id="t01">
+      <tr>
+<th>Müşteri Adı / e-mail</th>
+<th>Verilen Teklif Değeri</th>
+<th>Teklif Verilen Ürün</th>
+<th>Teklif Verilen Tarih</th>
+<th>Açık Arttırmanın Bitiş Tarihi</th>
+
+</tr>
+<tr>
+  <td>Hasan Mayok / hasnmyk47@gmail.com</td>
+  <td>50$</td>
+  <td>Collgate Diş Fırçası</td>
+  <td>21.03.2019</td>
+  <td>11.04.2019</td>
+</tr>
+<tr>
+  <td>Recep Erca / ercareco56@gmail.com</td>
+  <td>10$</td>
+  <td>Sporcu Kulaklığı</td>
+  <td>25.03.2019</td>
+  <td>15.04.2019</td>
+</tr>
+<tr>
+  <td>Tanser Sansa / tanserbey355@gmail.com</td>
+  <td>120$</td>
+  <td>Kırmızı Polo Sweat</td>
+  <td>20.03.2019</td>
+  <td>10.04.2019</td>
+</tr>
+<tr>
+  <td>Bulut Kaya / kayabulut1@gmail.com</td>
+  <td>44$ </td>
+  <td>Eversc ışıklı mouse</td>
+  <td>29.03.2019</td>
+  <td>9.04.2019</td>
+</tr>
+<tr>
+  <td>Cemre Gencer / gencersqs79@gmail.com </td>
+  <td>150$</td>
+  <td>Mavi dar pantolon</td>
+  <td>21.03.2019</td>
+  <td>07.04.2019</td>
+</tr>
+<tr>
+  <td>Hakan Daldan / dalanhakan@gmail.com</td>
+  <td>25$</td>
+  <td>Oyuncak konuşan ayı</td>
+  <td>26.03.2019</td>
+  <td>13.04.2019</td>
+</tr>
+    </table>
+  </body>
+</html>
+
